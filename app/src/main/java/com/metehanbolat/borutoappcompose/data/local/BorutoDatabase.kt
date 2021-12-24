8a8f5e0397faf8ba8cdb2cc6ba1fc4b350a24265
metehanie <metehan.bolat.ie@gmail.com>
2021-12-24 21:35:04 +0300

    DatabaseConverter class created.

--- app/src/main/java/com/metehanbolat/borutoappcompose/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/metehanbolat/borutoappcompose/data/local/BorutoDatabase.kt
@@ -2,12 +2,14 @@ package com.metehanbolat.borutoappcompose.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.metehanbolat.borutoappcompose.data.local.dao.HeroDao
 import com.metehanbolat.borutoappcompose.data.local.dao.HeroRemoteKeyDao
 import com.metehanbolat.borutoappcompose.domain.model.Hero
 import com.metehanbolat.borutoappcompose.domain.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(Database::class)
 abstract class BorutoDatabase: RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
--- app/src/main/java/com/metehanbolat/borutoappcompose/domain/model/HeroRemoteKey.kt
+++ app/src/main/java/com/metehanbolat/borutoappcompose/domain/model/HeroRemoteKey.kt
@@ -8,6 +8,6 @@ import com.metehanbolat.borutoappcompose.util.Constants.HERO_REMOTE_KEY_DATABASE
 data class HeroRemoteKey(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
-    val prevKey: Int?,
-    val nextKey: Int?
+    val prevPage: Int?,
+    val nextPage: Int?
 )
