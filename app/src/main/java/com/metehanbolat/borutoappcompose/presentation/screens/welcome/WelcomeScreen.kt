92b78421cdf75d61d16c5323663f38ad7977abc3
metehanie <metehan.bolat.ie@gmail.com>
2021-12-25 00:45:17 +0300

    Design Welcome Screen - 2

--- app/src/main/java/com/metehanbolat/borutoappcompose/presentation/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/metehanbolat/borutoappcompose/presentation/screens/welcome/WelcomeScreen.kt
@@ -1,18 +1,25 @@
 package com.metehanbolat.borutoappcompose.presentation.screens.welcome
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
+import com.metehanbolat.borutoappcompose.R
 import com.metehanbolat.borutoappcompose.domain.model.OnBoardingPage
-import com.metehanbolat.borutoappcompose.ui.theme.welcomeScreenBackgroundColor
+import com.metehanbolat.borutoappcompose.ui.theme.*
 import com.metehanbolat.borutoappcompose.util.Constants.ON_BOARDING_PAGE_COUNT
 
 @ExperimentalPagerApi
@@ -35,13 +42,69 @@ fun WelcomeScreen(navController: NavHostController) {
             state = pagerState,
             count = ON_BOARDING_PAGE_COUNT,
             verticalAlignment = Alignment.Top
-        ) { page ->
-             PagerScreen(onBoardingPage = pages[page])
+        ) { position ->
+             PagerScreen(onBoardingPage = pages[position])
         }
     }
 }
 
 @Composable
 fun PagerScreen(onBoardingPage: OnBoardingPage) {
-    
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = stringResource(R.string.on_boarding_image)
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = onBoardingPage.title,
+            color = MaterialTheme.colors.titleColor,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = EXTRA_LARGE_PADDING)
+                .padding(top = SMALL_PADDING),
+            text = onBoardingPage.description,
+            color = MaterialTheme.colors.descriptionColor,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FirstOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.First)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Second)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ThirdOnBoardingScreenPreview() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        PagerScreen(onBoardingPage = OnBoardingPage.Third)
+    }
 }
