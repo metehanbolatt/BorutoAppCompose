2a3b257de6db54bf67989afc29840061d126994b
metehanie <metehan.bolat.ie@gmail.com>
2021-12-26 00:35:35 +0300

    Create NetworkModule object

---
+++ app/src/main/java/com/metehanbolat/borutoappcompose/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.metehanbolat.borutoappcompose.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.metehanbolat.borutoappcompose.data.remote.BorutoApi
+import com.metehanbolat.borutoappcompose.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@ExperimentalSerializationApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.MINUTES)
+            .connectTimeout(15, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(retrofit: Retrofit): BorutoApi {
+        return retrofit.create(BorutoApi::class.java)
+    }
+}
