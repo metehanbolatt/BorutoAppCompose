10d169d02361c60e602c53189d4a5044fd04941d
metehanie <metehan.bolat.ie@gmail.com>
2021-12-25 23:21:07 +0300

    Home Screen - Design Top Bar

--- app/src/main/java/com/metehanbolat/borutoappcompose/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/metehanbolat/borutoappcompose/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,17 @@
 package com.metehanbolat.borutoappcompose.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {
 
+            })
+        }
+    ) {
+
+    }
 }
---
+++ app/src/main/java/com/metehanbolat/borutoappcompose/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,38 @@
+package com.metehanbolat.borutoappcompose.presentation.screens.home
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.metehanbolat.borutoappcompose.R
+import com.metehanbolat.borutoappcompose.ui.theme.topAppBarBackgroundColor
+import com.metehanbolat.borutoappcompose.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeTopBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Explore",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
