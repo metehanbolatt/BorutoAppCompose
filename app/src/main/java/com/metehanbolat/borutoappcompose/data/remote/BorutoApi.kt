bf3da0de250e50f91ffa56b847b4d23632896ffd
metehanie <metehan.bolat.ie@gmail.com>
2021-12-26 00:24:19 +0300

    Create ApiResponse & BorutoApi

---
+++ app/src/main/java/com/metehanbolat/borutoappcompose/data/remote/BorutoApi.kt
@@ -0,0 +1,18 @@
+package com.metehanbolat.borutoappcompose.data.remote
+
+import com.metehanbolat.borutoappcompose.domain.model.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BorutoApi {
+
+    @GET("/boruto/heroes")
+    suspend fun getAllHeroes(
+        @Query("page") page: Int = 1
+    ) : ApiResponse
+
+    @GET("/boruto/heroes/search")
+    suspend fun searchHeroes(
+        @Query("name") name: String
+    ) : ApiResponse
+}
