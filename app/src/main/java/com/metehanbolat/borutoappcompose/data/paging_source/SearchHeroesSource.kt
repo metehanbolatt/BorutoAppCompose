722ba4cb7f044558ed122622c5654429ab602547
metehanie <metehan.bolat.ie@gmail.com>
2021-12-31 03:15:11 +0300

    SearchScreen - Implement Search Heroes Source

---
+++ app/src/main/java/com/metehanbolat/borutoappcompose/data/paging_source/SearchHeroesSource.kt
@@ -0,0 +1,39 @@
+package com.metehanbolat.borutoappcompose.data.paging_source
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.metehanbolat.borutoappcompose.data.remote.BorutoApi
+import com.metehanbolat.borutoappcompose.domain.model.Hero
+import javax.inject.Inject
+
+class SearchHeroesSource @Inject constructor(
+    private val borutoApi: BorutoApi,
+    private val query: String
+): PagingSource<Int, Hero>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Hero> {
+        return try {
+            val apiResponse = borutoApi.searchHeroes(name = query)
+            val heroes = apiResponse.heroes
+            if (heroes.isNotEmpty()){
+                LoadResult.Page(
+                    data = heroes,
+                    prevKey = apiResponse.prevPage,
+                    nextKey = apiResponse.nextPage
+                )
+            }else{
+                LoadResult.Page(
+                    data = emptyList(),
+                    prevKey = null,
+                    nextKey = null
+                )
+            }
+        } catch (e: Exception){
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Hero>): Int? {
+        return state.anchorPosition
+    }
+}
